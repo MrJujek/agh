22ab9e69d4a92e5d09ff977df68e4cfcbcca610d
MrJujek <julian.dworzycki@gmail.com>
2025-09-04 20:02:19 +0200

    ✨ Maxrank

--- asd/old_colloquia/23-24/kol1/kol1.py
+++ asd/old_colloquia/23-24/kol1/kol1.py
@@ -1,8 +1,18 @@
 from kol1testy import runtests
 
 def maxrank(T):
-  # tu prosze wpisac wlasna implementacje
-  return -1
+  count = 0
+  maxi = 0
+
+  for i in range(len(T)):
+    count = 0
+    for j in range(i):
+      if T[j] < T[i]:
+        count += 1
+    
+    maxi = max(maxi, count)
+    
+  return maxi
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( maxrank, all_tests = False )
+runtests( maxrank, all_tests = True )
