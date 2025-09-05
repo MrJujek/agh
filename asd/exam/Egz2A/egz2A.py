a328b95ce614a479945281a8c1f623a86a3c0ab5
MrJujek <mr.jujek@gmail.com>
2025-09-05 13:45:37 +0200

    ✨ ASD 2 exam

--- asd/exam/Egz2A/egz2A.py
+++ asd/exam/Egz2A/egz2A.py
@@ -1,8 +1,67 @@
+# Julian Dworzycki
+# Złożoność obliczeniowa: O(D(V+E))
+# Tworzę zmienną successful_deliveries (którą zwrócę na koniec), która przechowuje liczbę udanych dostaw.
+# Zamieniam krawędzie grafu na reprezntację sąsiedztwa.
+# Każdego dnia sprawdzam czy jest dobra trasa dla kuriera.
+# Wykorzystuję do tego funckję find_path_bfs która jest bfsem z modyfikacją że sprawdzam czy sąsiad nie jest zablokowany.
+# Jak znalazłem trasę to zwracam True i zwiększam liczbę udanych dostaw.
+# Na koniec zwracam liczbę udanych dostaw.
+
 from egz2Atesty import runtests
+from collections import deque
 
 def kingnqueen( V,E,D,K,Q,B ):
-  # tu prosze wpisac wlasna implementacje
-  return -1
+  def find_path_bfs(adj, start, end, blocked):
+    if start == end:
+        return True
+
+    queue = deque([start])
+    visited = [False] * len(adj)
+    visited[start] = True
+
+    while queue:
+        current_city = queue.popleft()
+
+        if current_city == end:
+            return True
+
+        for neighbor in adj[current_city]:
+            if not visited[neighbor] and neighbor != blocked:
+                visited[neighbor] = True
+                queue.append(neighbor)
+
+    return False
+  
+  def edges_to_adjency_list(E, V):
+    adj = [[] for _ in range(V)]
+
+    for u,v in E:
+        adj[u].append(v)
+        adj[v].append(u)
+
+    return adj
+  
+  
+  succesful_deliveries = 0
+  adj = edges_to_adjency_list(E, V)
+
+  for i in range(D):
+      king_city = K[i]
+      queen_city = Q[i]
+      blocked_city = B[i]
+
+      if find_path_bfs(adj, king_city, queen_city, blocked_city):
+          succesful_deliveries += 1
+
+  return succesful_deliveries
+
+V = 9
+E = [(0, 1), (0, 2), (2, 1), (2, 3), (5, 2), (4, 2), (3, 4), (4, 5), (5, 6), (8, 6), (7, 8), (6, 7)]
+D = 3
+K = [0, 5, 7]
+Q = [3, 6, 0]
+B = [2, 2, 5]
+# print( kingnqueen( V, E, D, K, Q, B ) )
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( kingnqueen, all_tests = False )
+runtests( kingnqueen, all_tests = True )
--- asd/exam/Egz2B/egz2B.py
+++ asd/exam/Egz2B/egz2B.py
@@ -1,8 +1,36 @@
+# Julian Dworzycki
+# Złożoność obliczeniowa: O(n^2)
+# Tworzę tablicę ends w której będę przechowywał dobre końce odcinków.
+# Dla każdego elementu w T sprawdzam czy odcinek będzie kolidował z odcinkami w tablicy ends.
+# Sprawdzam to na kopii tablicy, gdyż jeżeli jest kolizja i usunę element z tablicy w trakcie bycia w bętli for to ten for nie ogarnia :c
+# Jeżeli była kolizja to usuwam wszytskie kolidujące końce z ends i nie dodaję nowego końca.
+# Jak nie było kolizji to dodaję koniec do ends.
+# Na koniec zwracam długość tablicy ends czyli liczbę dobrych odcinków. 
+
 from egz2Btesty import runtests
 
 def bitgame(T):
-  # tu prosze wpisac wlasna implementacje
-  return -1
+    ends = []
+    
+    for v in T:
+        colision = False
+        
+        cp = []
+        for u in ends:
+            cp.append(u)
+
+        for u in cp:
+            if u <= v:
+                ends.remove(u)
+                colision = True
+
+        if not colision:
+          ends.append(v)
+        
+    return len(ends)
+
+T = [5, 4, 3, 2, 4, 3, 1]
+# print(bitgame(T))
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( bitgame, all_tests = False )
+runtests( bitgame, all_tests = True )
