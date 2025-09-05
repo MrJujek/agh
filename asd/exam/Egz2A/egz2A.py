01109f3fb3828201a3723ddd13233f27175da398
MrJujek <mr.jujek@gmail.com>
2025-09-05 13:29:58 +0200

    🎉 Exam 2

---
+++ asd/exam/Egz2A/egz2A.py
@@ -0,0 +1,8 @@
+from egz2Atesty import runtests
+
+def kingnqueen( V,E,D,K,Q,B ):
+  # tu prosze wpisac wlasna implementacje
+  return -1
+
+# zmien all_tests na True zeby uruchomic wszystkie testy
+runtests( kingnqueen, all_tests = False )
---
+++ asd/exam/Egz2B/egz2B.py
@@ -0,0 +1,8 @@
+from egz2Btesty import runtests
+
+def bitgame(T):
+  # tu prosze wpisac wlasna implementacje
+  return -1
+
+# zmien all_tests na True zeby uruchomic wszystkie testy
+runtests( bitgame, all_tests = False )
