910cd183ab195708f54f2a9ec7ca61e85bd3c4a9
MrJujek <mr.jujek@gmail.com>
2026-03-16 22:34:33 +0100

    ✨ Calculate min and max

--- erl/src/ex3.erl
+++ erl/src/ex3.erl
@@ -10,7 +10,7 @@
 -author("julia").
 
 %% API
--export([getData/0, numberOfReadings/2]).
+-export([getData/0, numberOfReadings/2, calculateMinAndMax/2]).
 
 getData() ->
   [
@@ -23,8 +23,39 @@ getData() ->
 numberOfReadings(Readings, Date) ->
   numberOfReadings(Readings, Date, 0).
 
-numberOfReadings([], _, Acc) -> Acc;
+numberOfReadings([], _Date, Acc) -> Acc;
 numberOfReadings([{_, {Date, _}, _} | Tail], Date, Acc) ->
   numberOfReadings(Tail, Date, Acc + 1);
 numberOfReadings([_ | Tail], Date, Acc) ->
   numberOfReadings(Tail, Date, Acc).
+
+getValuesByType(Readings, Type) ->
+  getValuesByType(Readings, Type, []).
+
+getValuesByType([], _Type, Acc) ->
+  Acc;
+getValuesByType([{_, _, Measurements} | Tail], Type, Acc) ->
+  NewAcc = extractMeasurements(Measurements, Type, Acc),
+  getValuesByType(Tail, Type, NewAcc).
+
+extractMeasurements([], _Type, Acc) -> Acc;
+extractMeasurements([{Type, Value} | Tail], Type, Acc) ->
+  extractMeasurements(Tail, Type, [Value | Acc]);
+extractMeasurements([_ | Tail], Type, Acc) ->
+  extractMeasurements(Tail, Type, Acc).
+
+calculateMinAndMax(Readings, Type) ->
+  doCalculateMinAndMax(getValuesByType(Readings, Type)).
+
+doCalculateMinAndMax([]) -> {error, no_data_for_type};
+doCalculateMinAndMax([FirstValue | Rest]) ->
+  findMinMax(Rest, FirstValue, FirstValue).
+
+findMinMax([], Min, Max) ->
+  {Min, Max};
+findMinMax([Value | Tail], Min, Max) when Value < Min ->
+  findMinMax(Tail, Value, Max);
+findMinMax([Value | Tail], Min, Max) when Value > Max ->
+  findMinMax(Tail, Min, Value);
+findMinMax([_Value | Tail], Min, Max) ->
+  findMinMax(Tail, Min, Max).
